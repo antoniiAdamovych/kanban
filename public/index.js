7b22938ac4e2d5ab5b8befc1db0f9c312dc3d726
Antonii Adamovych <antoniiadamovych@gmail.com>
2024-05-27 17:13:15 +0200

    new slides

--- public/index.js
+++ public/index.js
@@ -6,6 +6,50 @@ import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/black.css';
 
 let deck = new Reveal({
-   plugins: [ Markdown ]
+   plugins: [Markdown]
 })
-deck.initialize();
+deck.initialize({
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+   InitToastActions();
+   KeyEvents();
+});
+
+function InitToastActions() {
+   const trigger = document.getElementById('toast_close');
+   const toast = document.querySelector('.toast');
+
+   trigger.onclick = () => toast.remove();
+
+   setTimeout(() => {
+      toast.style.opacity = 0;
+   }, 5000);
+}
+
+function KeyEvents() {
+   document.addEventListener(
+      "keydown",
+      (e) => {
+         if (e.key === "Enter") {
+            toggleFullScreen();
+         }
+      },
+      false,
+   );
+}
+
+function toggleFullScreen() {
+   const toast = document.querySelector('.toast');
+
+   if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+
+      if (toast) toast.style.opacity = 0;
+
+   } else if (document.exitFullscreen) {
+      document.exitFullscreen();
+      if (toast) toast.style.opacity = 1;
+   }
+}
